bdc146b98e577c1685efddf2db2a8f4fa12277ef
Matheus Baptista <matheushbm192@gmail.com>
2025-04-11 20:45:40 -0300

    Começando estudos sobre Árvores Binárias

---
+++ Arvore/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Arvore<Integer> arvore = new Arvore<>();
+        arvore.add(20);
+        arvore.add(15);
+        arvore.add(17);
+        arvore.add(25);
+        arvore.add(14);
+        arvore.add(18);
+    }
+}
